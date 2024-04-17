e817268a68e104de5f994c91bccda05fcdc9e12c
98ayoung <a01098619898@gmail.com>
2024-04-17 16:41:02 +0900

    02_intro page

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,3 +1,20 @@
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
 export function intro(){
+    gsap.registerPlugin(ScrollTrigger)
+    const frameCount=17
+    let OffsetValue=500
     
+    gsap.to('.img',{
+        backgroundPosition:(-OffsetValue*frameCount)+'px',
+        ease:'steps('+frameCount+')',
+        scrollTrigger:{
+            trigger:'#intro',
+            start:'top top',
+            end:'+='+(frameCount*OffsetValue),
+            pin:true,  
+            scrub:true
+        }
+    })
 }
