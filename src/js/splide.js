6de0f6042d33b22812a71d32a8771f3f30ea2c9c
98ayoung <a01098619898@gmail.com>
2024-04-24 15:15:59 +0900

    04 about me

--- src/js/splide.js
+++ src/js/splide.js
@@ -16,7 +16,7 @@ export function splide(){
     }).mount({AutoScroll});
 
     new Splide( '#splide2',{
-        direction : 'ltl',
+        direction : 'rtl',
         type : "loop",
         autoWidth : true,
         focus : 'center',
